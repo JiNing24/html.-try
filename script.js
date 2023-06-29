61645cc5fafe3191dc2e066f71e70a693a8a37fc
JiNing24 <132535252+JiNing24@users.noreply.github.com>
2023-06-30 01:19:31 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let menu = document.querySelector('#meno-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', { delay: 200, origin: 'top' });
+sr.reveal('.hero-img', { delay: 450, origin: 'top' });
+
+const navigation = document.querySelector(".primary-navigation");
+const navigationHeight = navigation.offsetHeight;
+document.documentElement.style.setProperty(
+    "--scroll-padding",
+    navigationHeight + "px"
+);
